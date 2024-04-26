8e6ebc97b615972534fd6a1a958f9f24ac2b74b0
asur <asur_xd@hotmail.com>
2024-04-26 11:33:42 -0600

    dependences depercated

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,5 +1,4 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouer = GoRouter(initialLocation: '/', routes: [
